a0509aa15789eee9ca8d5e8bd97662c3fa1984e3
518H0062 <62529614+518H0062@users.noreply.github.com>
2021-06-28 17:18:10 +0700

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -3,8 +3,8 @@ module.exports = {
     // googleClientID:"525179804393-gr9d9g233ag1977naocclsn0m2j4kpft.apps.googleusercontent.com",
     // googleClientSecret: "MPEpDkTIgKPgzYmUUt1J2sFS",
     
-    googleClientID:"388413912899-j6ic4u54adip4fijt2p6insg4elhf8fm.apps.googleusercontent.com",  
-    googleClientSecret: "FTJXV-CaPcB6_RCPCjFdes0i",
+    googleClientID:"674482083620-o8n0f3oo7vjllhj7b10h7o4pr7k1vv6r.apps.googleusercontent.com",  
+    googleClientSecret: "nbdvF9C7b6A2QAgsz_XYtko7",
 
     //mongoURI: "mongodb://CSC430-1:Dy1y5YpvyyVXOVm1Quw5@ds163918.mlab.com:63918/udemy-test" ,
     mongoURI: "mongodb+srv://user:21012014@cluster0.xp2br.mongodb.net/user?retryWrites=true&w=majority" ,
